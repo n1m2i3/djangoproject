966c933c175cdbe7caff06835a7e418c59d757ab
amisalem91993 <amisalem91993@gmail.com>
2026-02-06 15:57:27 +0100

    Voeg Voetbalspelers-model toe en pas admin.py aan

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,20 +2,15 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
-class Post(models.Model):
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
-
+class Voetbalspelers(models.Model):
+    naam_voetballer = models.CharField(max_length=200)
+    actuele_voetbalclub = models.CharField(max_length=200)
+    auteur = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    datum_invoer = models.DateTimeField(default=timezone.now)
+    datum_laatste_aanpassing = models.DateTimeField(blank=True, null=True)
+    
     def publish(self):
-        self.published_date = timezone.now()
         self.save()
-
+    
     def __str__(self):
-        return self.title
+        return self.naam_voetballer
